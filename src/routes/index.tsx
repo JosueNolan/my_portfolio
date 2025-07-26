80546b4ffac88172585e3a8ed3abb7674efea022
josue_nolan <josuenolan96@gmail.com>
2025-07-26 01:26:13 -0600

    cambio browser router oor hash router por problema de github pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import MainLayout from "@/components/MainLayout"
 import {
-	createBrowserRouter,
+	createHashRouter,
 	createRoutesFromElements,
 	Route,
 } from "react-router"
 import DefaultPage from "./DefaultPage"
 
-const router = createBrowserRouter(
+const router = createHashRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<MainLayout />}>
 			<Route index element={<DefaultPage />} />
