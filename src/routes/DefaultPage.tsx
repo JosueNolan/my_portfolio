1bc820e0128a5716a4430fdc4372c3e031395e2a
josue_nolan <josuenolan96@gmail.com>
2025-07-26 00:54:21 -0600

    Se corrigen errores en el deploy

--- src/routes/DefaultPage.tsx
+++ src/routes/DefaultPage.tsx
@@ -1,11 +1,7 @@
 import AboutMe from "@/components/default/AboutMe"
 import Projects from "@/components/default/Projects"
-import { useTheme } from "@/components/ThemeProvider"
-import { useTranslation } from "react-i18next"
 
 const DefaultPage = () => {
-	const { t } = useTranslation()
-	const { theme } = useTheme()
 	return (
 		<div className="min-h-[80dvh]">
 			<AboutMe />
